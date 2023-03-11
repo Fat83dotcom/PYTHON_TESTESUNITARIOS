90c1c3211a0da05c60aee143e941f6e9a7a65879
fat83dotcom <fernando.quimic@gmail.com>
2023-03-10 21:28:27 -0300

    Implementação da função - a criação dessa função foi feita usando metodos básicos de TDD -Inicialmete os teste foram criados primeiro

--- baconcomovos.py
+++ baconcomovos.py
@@ -13,3 +13,17 @@ retorna: Ovos
 5 - Saber se o número não é multiplo de 3 e 5
 retona : Passa fome
 '''
+
+
+def baconComOvos(n):
+    try:
+        assert isinstance(n, int), "n deve ser 'int'"
+        if n % 3 == 0 and n % 5 == 0:
+            return "Bacon Com Ovos"
+        if n % 3 == 0:
+            return "Bacon"
+        if n % 5 == 0:
+            return "Ovos"
+        return "Passar fome"
+    except AssertionError as e:
+        raise e
